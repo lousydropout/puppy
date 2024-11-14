b22539d8aea397fd382e4b590a929fc575adf7f5
lousydropout <lousydropout@gmail.com>
2024-11-13 22:00:22 -0800

    reduce width

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,8 @@
-import { createEffect, onMount } from "solid-js";
+import { onMount } from "solid-js";
 import { animate, queueAction } from "./utils/animation";
 import { actions } from "./utils/actions";
-import { transcript } from "./store";
-import {
-  isRecording,
-  setIsRecording,
-  setSttCompatible,
-  setTranscript,
-} from "./store";
 
 export const App = () => {
-  setSttCompatible(
-    "SpeechRecognition" in window || "webkitSpeechRecognition" in window
-  );
-
-  const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
-  recognition.continuous = false;
-  recognition.interimResults = false;
-  recognition.lang = "en-US";
-
-  recognition.onresult = (event: any) => {
-    console.log("[recognition.onresult] event: ", event);
-    setIsRecording(false);
-
-    const speechResult = Array.from(event.results)
-      .map((result: any) => result[0].transcript)
-      .join("");
-    console.log("speechResult: ", speechResult);
-    setTranscript(speechResult);
-  };
-
-  const startRecording = () => {
-    console.log("down");
-    setIsRecording(true);
-    console.log("isRecording: ", isRecording());
-    recognition.start();
-  };
-
-  const stopRecording = () => {
-    console.log("up");
-    setIsRecording(false);
-    console.log("isRecording: ", isRecording());
-  };
-
-  const root = document.getElementById("root");
-  root?.addEventListener("mousedown", startRecording);
-  root?.addEventListener("mouseup", stopRecording);
-
   let canvasRef: HTMLCanvasElement | undefined;
 
   onMount(() => {
@@ -59,25 +13,10 @@ export const App = () => {
     animate(ctx, actions.normal);
   });
 
-  createEffect(() => {
-    const text = transcript().toLowerCase();
-
-    switch (text) {
-      case "good puppy":
-        queueAction("happy");
-        setTranscript("");
-        break;
-      // Add more cases as needed
-      default:
-        console.log("No matches found for: ", text);
-    }
-  });
-
   return (
-    <div class="flex-col items-center pt-8 min-h-screen min-w-[500px]">
-      <canvas class="mx-auto" width={700} height={500} ref={canvasRef}></canvas>
-      {/* <SpeechToText />
-      <h1 class="text-center">Transcript: {transcript() || "nothing"}</h1> */}
+    <div class="flex-col items-center pt-8 min-h-screen">
+      <canvas class="mx-auto" width={400} height={300} ref={canvasRef}></canvas>
+      <h1 class="text-center">Puppy</h1>
       <button
         id="happy-btn"
         class={`bg-slate-300 hover:bg-slate-400 active:bg-slate-500 active:text-white
--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,20 +1,32 @@
 import { ActionName, actions, pendingQueue, type Action } from "./actions";
 
 const offset = 80;
-const CANVAS_WIDTH = 700;
-const CANVAS_HEIGHT = 500;
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 300;
 
 const SPRITE_WIDTH = 688;
 const SPRITE_HEIGHT = 432;
 let gameFrame = 0;
 let k = 0;
 
-const drawFrame = (ctx: CanvasRenderingContext2D, frameX: number, img: HTMLImageElement) => {
+const drawFrame = (
+  ctx: CanvasRenderingContext2D,
+  frameX: number,
+  img: HTMLImageElement
+) => {
   if (!ctx) return;
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(img,
-    frameX * SPRITE_WIDTH + offset, 0, SPRITE_WIDTH - offset, SPRITE_HEIGHT,
-    0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(
+    img,
+    frameX * SPRITE_WIDTH + offset,
+    0,
+    SPRITE_WIDTH - offset,
+    SPRITE_HEIGHT,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
 };
 
 export const animate = (ctx: CanvasRenderingContext2D, action: Action) => {
