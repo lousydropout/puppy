c2a14dade6c556cd3b1f5113c7bba61ba295124b
lousydropout <lousydropout@gmail.com>
2024-11-19 20:53:17 -0800

    add button to let user mute/unmute bg music

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { onMount } from "solid-js";
+import { createEffect, createSignal, onMount, Show } from "solid-js";
 import { animate, queueAction } from "./utils/animation";
 import { actions } from "./utils/actions";
+import { Sound } from "./icons/Sound";
+import { Muted } from "./icons/Muted";
 
 export const App = () => {
+  const [muted, setMuted] = createSignal(true);
+
   let canvasRef: HTMLCanvasElement | undefined;
   const bgMusic = new Audio("/level-7-27947.mp3");
   bgMusic.loop = true;
@@ -13,25 +17,41 @@ export const App = () => {
     if (!ctx) return;
     ctx.globalAlpha = 1;
     animate(ctx, actions.normal);
+  });
 
-    bgMusic.play();
+  createEffect(() => {
+    if (muted()) {
+      bgMusic.pause();
+    } else {
+      bgMusic.play();
+    }
   });
 
   return (
-    <div class="flex flex-col items-center justify-start py-16 gap-16 min-h-screen">
-      <h1 class="text-7xl font-semibold italic text-center">Puppy</h1>
-      <div class="flex flex-col">
-        <canvas width={400} height={300} ref={canvasRef}></canvas>
-        <button
-          class={`bg-slate-300
+    <>
+      <button
+        onClick={() => setMuted((prev) => !prev)}
+        class="absolute top-0 right-0 p-4 cursor-pointer"
+      >
+        <Show when={muted()} fallback={<Sound />}>
+          <Muted />
+        </Show>
+      </button>
+      <div class="flex flex-col items-center justify-start py-16 gap-16 min-h-screen">
+        <h1 class="text-7xl font-semibold italic text-center">Puppy</h1>
+        <div class="flex flex-col">
+          <canvas width={400} height={300} ref={canvasRef}></canvas>
+          <button
+            class={`bg-slate-300
           hover:bg-slate-400 hover:text-white
           active:bg-slate-500 active:text-white
           border rounded-lg px-5 py-3 block w-fit mx-auto mt-4`}
-          onClick={() => queueAction("happy")}
-        >
-          "Good puppy!"
-        </button>
+            onClick={() => queueAction("happy")}
+          >
+            "Good puppy!"
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
