3b6b0e46d38ebbb66da7ef6ecc3ab6db35185c90
lousydropout <lousydropout@gmail.com>
2024-11-25 16:50:53 -0800

    test how vercel's environment variables work

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,6 +1,7 @@
 "use server";
+
 export const callGoogle = async () => {
   const response = await fetch("https://www.google.com?secret=s123");
   console.log("Called Google");
-  return await response.text();
+  return process.env.CLIENT_KEY;
 };
